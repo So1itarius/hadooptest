5e0e68f76139657f9faa3492303688be2de35c46
Your Name <usertest@mail.com>
2019-12-25 15:14:35 +0300

    day3

--- src/TestMain.java
+++ src/TestMain.java
@@ -1,20 +1,84 @@
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class TestMain {
+    protected static ArrayList<String> Counter(ArrayList<String> fakeArray) {
+        Map<String, Integer> letters = new HashMap<>();
+
+        for (int i = 0; i < fakeArray.size(); i++) {
+            String tempChar = fakeArray.get(i);
+
+            if (!letters.containsKey(tempChar)) {
+                letters.put(tempChar, 1);
+            } else {
+                letters.put(tempChar, letters.get(tempChar) + 1);
+            }
+        }
+        int max = 0;
+        String maxStr="";
+        for (Map.Entry<String, Integer> entry : letters.entrySet()) {
+            if (entry.getValue()>max){maxStr=entry.getKey();max=entry.getValue();}
+            //System.out.println("key " + entry.getKey() + ",value " + entry.getValue());
+        }
+        //System.out.println(max);
+        //System.out.println(maxStr);
+        ArrayList<String> fakeArray1 = new ArrayList<>();
+        fakeArray1.add(maxStr);
+        fakeArray1.add(String.valueOf(max));
+        return fakeArray1;
+
+    }
+    protected static String finder(String str) {
+        Pattern pattern = Pattern.compile("\\d+/\\d+");
+        Matcher matcher = pattern.matcher(str);
+        while(matcher.find()){
+           str = matcher.group();
+        }
+        //return matcher.group();
+        return str;
+    }
+    protected static String rep1(String str) {
+        Pattern pattern1 = Pattern.compile("\"\\d+,\\d+\"");
+        Matcher matcher1 = pattern1.matcher(str);
+        String s = "";
+        while(matcher1.find())
+            s = matcher1.group().replace("\"","").replace(",","");
+        String myStr =str.replaceAll("\"\\d+,\\d+\"", s);
+        //System.out.println(myStr);
+        return myStr;
+    }
+
     public static void main(String[] args) throws ParseException {
-       String s = "2019-11-18 09:47:28.525000";
-
-
+        String s = "2019-11-18 09:47:28.525000";
         DateFormat dateFormat = new SimpleDateFormat("YYYY-MM-dd HH:mm:ss");
         Date date = dateFormat.parse(s);
-        long unixTime = date.getTime()/1000;
-        System.out.println(unixTime);
-
-
-
-
+        long unixTime = date.getTime() / 1000;
+        //System.out.println(unixTime);
+        ArrayList<String> a = new ArrayList<String>();
+        a.add("Visa");a.add("Visa");a.add("Mastercard");a.add("Mastercard");a.add("Mastercard");
+        //(Visa, Visa, Mastercard, Mastercard, Mastercard)
+        //System.out.println(Counter(a));
+        String s1 = "1/8/09 13:14";
+        //String input = "Hello Java! Hello JavaScript! JavaSE 8.";
+        Pattern pattern = Pattern.compile("\\d+/\\d+");
+        Matcher matcher = pattern.matcher(s1);
+        //System.out.println(matcher.group());
+        //while(matcher.find())
+        //    System.out.println(matcher.group());
+        //System.out.println(finder(s1));
+        String s2 = "1/28/09 18:00,Product1,13000,Visa,sandhya,Centennial                  ,CO,United States,12/2/06 23:24,2/7/09 15:18,39.57917,-104.87639";
+        Pattern pattern1 = Pattern.compile("\"\\d+,\\d+\"");
+        Matcher matcher1 = pattern1.matcher(s2);
+        //System.out.println(matcher.group());
+        //while(matcher1.find())
+        //   System.out.println(matcher1.group().replace("\"","").replace(",",""));
+        System.out.println(rep1(s2));
     }
 }
--- src/WordCountJob.java
+++ src/WordCountJob.java
@@ -8,10 +8,7 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
-import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.MultipleOutputs;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
@@ -29,26 +26,35 @@ public class WordCountJob {
         if (hdfs.exists(output)) {
             hdfs.delete(output, true);
         }
-        HashMap<String, String> allUsers = new HashMap<String, String>();
+        //HashMap<String, String> allUsers = new HashMap<String, String>();
         Job job = Job.getInstance();
         job.setJarByClass(WordCountJob.class);
-        //TextInputFormat.addInputPath(job, new Path("logs_example.csv"));
-        MultipleInputs.addInputPath(job, new Path("users.csv"), TextInputFormat.class, UserMapper.class);
-        MultipleInputs.addInputPath(job, new Path("departments.csv"), TextInputFormat.class, DepartmentMapper.class);
+        TextInputFormat.addInputPath(job, new Path("C:\\Users\\User\\IdeaProjects\\hadooptest\\SalesJan2009.csv"));
         job.setInputFormatClass(TextInputFormat.class);
+        //job.setMapperClass(CountryMapper.class);
+        //job.setReducerClass(CountryReducer.class);
+        job.setMapperClass(AVGMapper.class);
+        job.setReducerClass(AVGReducer.class);
+        //MultipleInputs.addInputPath(job, new Path("users.csv"), TextInputFormat.class, UserMapper.class);
+        //MultipleInputs.addInputPath(job, new Path("departments.csv"), TextInputFormat.class, DepartmentMapper.class);
+
         //job.setMapperClass(UniqueUserMapper.class);
         //job.setReducerClass(UniqueUserReducer.class);
         //job.setMapperClass(UserMapper.class);
         //job.setMapperClass(DepartmentMapper.class);
-        job.setReducerClass(MultipleOutputsReducer.class);
+        //job.setReducerClass(MultipleOutputsReducer.class);
         //TextOutputFormat.setOutputPath(job, new Path("doc2.txt"));
         //job.setInputFormatClass(TextInputFormat.class);
+
+
 
         //FileInputFormat.setInputPaths(job, new Path("doc2.txt"));
         TextOutputFormat.setOutputPath(job, new Path("doc2.txt"));
         job.setOutputFormatClass(TextOutputFormat.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);
+        job.setMapOutputKeyClass(Text.class);
+        //job.setMapOutputValueClass(DataWritable.class);
         System.exit(job.waitForCompletion(true) ? 0 : 1);
 
     }
@@ -102,6 +108,111 @@ public class WordCountJob {
             }
             context.write(key, new Text(tempString.toString()));
 
+        }
+    }
+    public static class CountryMapper extends Mapper<LongWritable, Text, Text, Text> {
+        @Override
+        protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            if (key.equals(new LongWritable(0))){
+                return;
+            }
+            String[] tokens = value.toString().split(",");
+            context.write(new Text(tokens[7]), new Text(tokens[3]));
+        }
+    }
+
+    public static class CountryReducer extends Reducer<Text, Text, Text, Text> {
+        @Override
+        protected void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+
+            ArrayList<String> tempString = new ArrayList<String>();
+            int flag = 0;
+            HashMap<String, ArrayList<String>> dict = new HashMap<String, ArrayList<String>>();
+            ArrayList<String> a;
+            for (Text value : values) {
+                //System.out.println("key: " + key);
+                //System.out.println(value.toString());
+                if (flag==0){dict.put(key.toString(),new ArrayList<>());flag=1;}
+                //dict.put(key.toString(),new ArrayList<>());
+                a = dict.get(key.toString());
+                a.add(value.toString());
+                dict.put(key.toString(),a);
+                tempString.add(value.toString());
+                tempString.add(String.valueOf(flag));
+                flag++;
+            }
+            for (Map.Entry entry : dict.entrySet()) {
+                System.out.println("Key: " + entry.getKey() + " Value: "
+                        + TestMain.Counter((ArrayList<String>) entry.getValue()));
+                dict.put((String) entry.getKey(),TestMain.Counter((ArrayList<String>) entry.getValue()));
+            }
+            //for (ArrayList<String> value : dict.values()) {
+            //    System.out.println("Value: " + TestMain.Counter(value));
+            //}
+            //for (Map.Entry entry : dict.entrySet()) {
+            //    dict.put((String) entry.getKey(), TestMain.Counter((ArrayList<String>) entry.getValue()));
+            //}
+            context.write(new Text("Null"),new Text(dict.toString()));
+        }
+    }
+    public static class AVGMapper
+            extends Mapper<LongWritable, Text, Text, Text> {
+        private final static IntWritable one = new IntWritable(1);
+        private final Text word = new Text();
+
+        @Override
+        protected void map(LongWritable key, Text value, Context context)
+                throws IOException, InterruptedException {
+            if (key.equals(new LongWritable(0))){
+                return;
+            }
+            //if
+            String[] kosmos = TestMain.rep1(value.toString()).split(",");
+            //context.write(new Text(kosmos[4]),new Text(kosmos[2]));
+            context.write(new Text(kosmos[7]+" "+ TestMain.finder(kosmos[0])), new Text(kosmos[2]));
+
+        }
+    }
+
+    public static class AVGReducer
+            extends Reducer<Text, Text, Text, Text> {
+        protected void reduce(Text key, Iterable<Text> values,
+                              Context context)
+                throws IOException, InterruptedException {
+            ArrayList<String> tempString = new ArrayList<String>();
+            //for (Text value : values) {
+            //    tempString.add(value.toString());
+            //}
+            int flag = 0;
+            HashMap<String, ArrayList<String>> dict = new HashMap<String, ArrayList<String>>();
+            ArrayList<String> a;
+            for (Text value : values) {
+                //System.out.println("key: " + key);
+                //System.out.println(value.toString());
+                if (flag==0){dict.put(key.toString(),new ArrayList<>());flag=1;}
+                //dict.put(key.toString(),new ArrayList<>());
+                a = dict.get(key.toString());
+                a.add(value.toString());
+                dict.put(key.toString(),a);
+                tempString.add(value.toString());
+                //tempString.add(String.valueOf(flag));
+                flag++;
+            }
+            HashMap<String, Double> dict1 = new HashMap<>();
+            double a1;
+            for (Map.Entry entry : dict.entrySet()) {
+                //System.out.println("Key: " + entry.getKey() + " Value: "
+                //        + ((ArrayList<String>) entry.getValue()).stream().mapToInt((s) -> Integer.parseInt(s)).average());
+                a1 = ((ArrayList<String>) entry.getValue()).stream().mapToInt((s) -> Integer.parseInt(s)).average().getAsDouble();
+                dict1.put((String) entry.getKey(), a1);
+            }
+            /*for (Map.Entry entry : dict1.entrySet()) {
+                System.out.println("Key: " + entry.getKey() + " Value: "
+                        + ((ArrayList<String>) entry.getValue()).stream().mapToInt((s) -> Integer.parseInt(s)).average());
+                //a1 = ((ArrayList<String>) entry.getValue()).stream().mapToInt((s) -> Integer.parseInt(s)).average();
+                //dict1.put((String) entry.getKey(), a1);
+            }*/
+            context.write(new Text("Null"), new Text(dict1.toString()));
         }
     }
 
